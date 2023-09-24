073d4f0d15f1aab38382d274221481dd2fb3cfa7
Na-Okamoto <91584390+Na-Okamoto@users.noreply.github.com>
2023-09-24 14:08:18 +0900

    simple sample code

--- sensor_sample/sensor_sample.ino
+++ sensor_sample/sensor_sample.ino
@@ -2,20 +2,13 @@
 #include "ESC2nd.h"
 
 #define LPF(obs, x_prev, alpha) ((1 - alpha) * x_prev + obs * alpha)
-#define GREEN_THRESHOLD (20)
-#define GREEN_CH (3)
-#define BUZZER_PIN (8)
 
 // SensorクラスのインスタンスをDEVICE_ADDR1に対応するADCで作成
-Sensor sensor(DEVICE_ADDR2);
-Motor motorR('r');
-Motor motorL('l');
-float filterd_value[8] = {0,0,0,0,0,0,0,0};
+Sensor sensor(DEVICE_ADDR1);
 
 
 // プログラムがスタートすると一度だけ実行される
 void setup(){
-  pinMode(BUZZER_PIN, OUTPUT);
   // PCとarduinoのデータ通信速度を設定(9600bps)
   Serial.begin(9600);
   // 初期化が終わる前に動き出さないように1000ms待つ
@@ -28,22 +21,13 @@ void loop(){
   int value[8];
   // センサーから読み出してvalに格納
   sensor.read(value);
-  motorR.drive(100);
-  motorL.drive(100);
 
   // val[0]からval[7]までの値を出力
   for(int i=0;i<8;i++){
-  filterd_value[i] = LPF(value[i], filterd_value[i], 0.9);
-   Serial.print(filterd_value[i]);
+   Serial.print(value[i]);
    Serial.print(" ");
   }
 
-  if(filterd_value[GREEN_CH] > GREEN_THRESHOLD){
-    digitalWrite(BUZZER_PIN, HIGH);
-  }else{
-    digitalWrite(BUZZER_PIN, LOW);
-
-  }
   // 改行
   Serial.println();
   // 100ms待つ
